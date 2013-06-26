a2d2afb27286e5955d49ff941e7f26ede66f150f
Matthieu Aussaguel <matthieu.aussaguel@gmail.com>
2013-06-26 19:52:33 +0200

    WIP Flip update

--- app/controllers/flips_controller.rb
+++ app/controllers/flips_controller.rb
@@ -18,6 +18,19 @@ class FlipsController < ApplicationController
     end
   end
 
+  def edit
+    @flip = Flip.find params[:id]
+  end
+
+  def update
+    if @flip.update_attributes(flip_params)
+      redirect_to redirect_to flips_path
+    else
+      render :edit
+    end
+
+  end
+
   private
 
   def flip_params
